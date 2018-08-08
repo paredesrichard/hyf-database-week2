a5557b5c1663d11574f1f79e8752f94dde886d2e
Richard Paredes <chard.paredes@gmail.com>
2018-08-08 20:08:13 +0200

    added error handling

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-var fs = require('fs');
-var mysql = require('mysql');
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
 
-var config = JSON.parse(fs.readFileSync("config-secret.json"))
+const fs = require("fs");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    port: config.port,
-    database: config.database
+const config = JSON.parse(fs.readFileSync("config-secret.json"));
+
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  port: config.port,
+  database: config.database
 });
 
 connection.connect();
 
-connection.query('SELECT * FROM user limit 0,5', function(error, results, fields) {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM `user`", function(error, results, fields) {
+    if (error) throw error;
     console.log(results);
+    res.send(results);
+  });
 });
 
-connection.end();
+app.get("/users/:id", (req, res) => {
+  console.log('id parameter:', req.params.id);
+  connection.query("SELECT * FROM `user` WHERE `id` = ?", req.params.id,  function(error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    res.send(results);
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000");
+});
